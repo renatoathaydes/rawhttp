7b64f84feedb7d3b11fee249efdd5ef93b41844a
renatoathaydes <renatoathaydes@hotmail.com>
2017-12-02 22:22:06 +0100

    Removed Socket constructor from TcpRawHttpClient. Manage internally sockets as necessary.

--- rawhttp-core/src/main/java/com/athaydes/rawhttp/core/client/TcpRawHttpClient.java
+++ rawhttp-core/src/main/java/com/athaydes/rawhttp/core/client/TcpRawHttpClient.java
@@ -5,21 +5,43 @@ import com.athaydes.rawhttp.core.RawHttpClient;
 import com.athaydes.rawhttp.core.RawHttpRequest;
 import com.athaydes.rawhttp.core.RawHttpResponse;
 
+import javax.annotation.Nullable;
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
-public class TcpRawHttpClient implements RawHttpClient<Void> {
+public class TcpRawHttpClient implements RawHttpClient<Void>, Closeable {
 
-    private final Socket socket;
-
-    public TcpRawHttpClient(Socket socket) {
-        this.socket = socket;
-    }
+    private final Map<String, Socket> socketByHost = new HashMap<>(4);
 
     @Override
     public RawHttpResponse<Void> send(RawHttpRequest request) throws IOException {
+        String host = request.getUri().getHost();
+        @Nullable Socket socket = socketByHost.get(host);
+
+        if (socket == null || socket.isClosed() || !socket.isConnected()) {
+            int port = request.getUri().getPort();
+            if (port == -1) {
+                port = "https".equalsIgnoreCase(request.getUri().getScheme()) ?
+                        43 : 80;
+            }
+            socket = new Socket(host, port);
+            socketByHost.put(host, socket);
+        }
+
         request.writeTo(socket.getOutputStream());
         return new RawHttp().parseResponse(socket.getInputStream());
     }
 
+    public void close() {
+        for (Socket socket : socketByHost.values()) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- rawhttp-core/src/test/kotlin/com/athaydes/rawhttp/core/TcpRawHttpClientTests.kt
+++ rawhttp-core/src/test/kotlin/com/athaydes/rawhttp/core/TcpRawHttpClientTests.kt
@@ -10,7 +10,6 @@ import spark.Spark.get
 import spark.Spark.port
 import spark.Spark.post
 import spark.Spark.stop
-import java.net.Socket
 import java.nio.charset.StandardCharsets.UTF_8
 
 fun sparkServerInterceptor(spec: Spec, runTest: () -> Unit) {
@@ -35,42 +34,36 @@ class TcpRawHttp10ClientTest : StringSpec() {
 
     init {
         "Must be able to perform a simple HTTP 1.0 request against a real HTTP server" {
-            Socket("localhost", 8083).use { socket ->
-                TcpRawHttpClient(socket).send(RawHttp().parseRequest(
-                        "GET http://localhost:8083/say-hi HTTP/1.0")).eagerly().run {
-                    body should bePresent {
-                        it.asString(UTF_8) shouldBeOneOf setOf("Hi there", "{ \"message\": \"Hi there\" }")
-                    }
+            TcpRawHttpClient().send(RawHttp().parseRequest(
+                    "GET http://localhost:8083/say-hi HTTP/1.0")).eagerly().run {
+                body should bePresent {
+                    it.asString(UTF_8) shouldBeOneOf setOf("Hi there", "{ \"message\": \"Hi there\" }")
                 }
             }
         }
 
         "Must be able to perform a HTTP 1.0 request with headers against a real HTTP server" {
-            Socket("localhost", 8083).use { socket ->
-                TcpRawHttpClient(socket).send(RawHttp().parseRequest(
-                        "GET http://localhost:8083/say-hi HTTP/1.0\r\n" +
-                                "Host: localhost\r\n" +
-                                "Accept: text/plain")).eagerly().run {
-                    body should bePresent {
-                        it.asString(UTF_8) shouldBe "Hi there"
-                    }
+            TcpRawHttpClient().send(RawHttp().parseRequest(
+                    "GET http://localhost:8083/say-hi HTTP/1.0\r\n" +
+                            "Host: localhost\r\n" +
+                            "Accept: text/plain")).eagerly().run {
+                body should bePresent {
+                    it.asString(UTF_8) shouldBe "Hi there"
                 }
             }
         }
 
         "Must be able to perform a HTTP 1.0 request with headers and a body against a real HTTP server" {
-            Socket("localhost", 8083).use { socket ->
-                TcpRawHttpClient(socket).send(RawHttp().parseRequest(
-                        "POST http://localhost:8083/echo HTTP/1.0\r\n" +
-                                "Host: localhost\r\n" +
-                                "Accept: text/plain\r\n" +
-                                "Content-Type: text/plain\r\n" +
-                                "Content-Length: 11\r\n" +
-                                "\r\n" +
-                                "hello world")).eagerly().run {
-                    body should bePresent {
-                        it.asString(UTF_8) shouldBe "hello world"
-                    }
+            TcpRawHttpClient().send(RawHttp().parseRequest(
+                    "POST http://localhost:8083/echo HTTP/1.0\r\n" +
+                            "Host: localhost\r\n" +
+                            "Accept: text/plain\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "Content-Length: 11\r\n" +
+                            "\r\n" +
+                            "hello world")).eagerly().run {
+                body should bePresent {
+                    it.asString(UTF_8) shouldBe "hello world"
                 }
             }
         }
@@ -86,42 +79,37 @@ class TcpRawHttp11ClientTest : StringSpec() {
 
     init {
         "Must be able to perform a simple HTTP 1.1 request against a real HTTP server" {
-            Socket("localhost", 8083).use { socket ->
-                TcpRawHttpClient(socket).send(RawHttp().parseRequest(
-                        "GET http://localhost:8083/say-hi HTTP/1.1")).eagerly().run {
-                    body should bePresent {
-                        it.asString(UTF_8) shouldBeOneOf setOf("Hi there", "{ \"message\": \"Hi there\" }")
-                    }
+            TcpRawHttpClient().send(RawHttp().parseRequest(
+                    "GET http://localhost:8083/say-hi HTTP/1.1")).eagerly().run {
+                body should bePresent {
+                    it.asString(UTF_8) shouldBeOneOf setOf("Hi there", "{ \"message\": \"Hi there\" }")
                 }
             }
+
         }
 
         "Must be able to perform a HTTP 1.1 request with headers against a real HTTP server" {
-            Socket("localhost", 8083).use { socket ->
-                TcpRawHttpClient(socket).send(RawHttp().parseRequest(
-                        "GET http://localhost:8083/say-hi HTTP/1.1\r\n" +
-                                "Host: localhost\r\n" +
-                                "Accept: text/plain")).eagerly().run {
-                    body should bePresent {
-                        it.asString(UTF_8) shouldBe "Hi there"
-                    }
+            TcpRawHttpClient().send(RawHttp().parseRequest(
+                    "GET http://localhost:8083/say-hi HTTP/1.1\r\n" +
+                            "Host: localhost\r\n" +
+                            "Accept: text/plain")).eagerly().run {
+                body should bePresent {
+                    it.asString(UTF_8) shouldBe "Hi there"
                 }
             }
         }
 
         "Must be able to perform a HTTP 1.1 request with headers and a body against a real HTTP server" {
-            Socket("localhost", 8083).use { socket ->
-                TcpRawHttpClient(socket).send(RawHttp().parseRequest(
-                        "POST http://localhost:8083/echo HTTP/1.1\r\n" +
-                                "Host: localhost\r\n" +
-                                "Accept: text/plain\r\n" +
-                                "Content-Type: text/plain\r\n" +
-                                "Content-Length: 11\r\n" +
-                                "\r\n" +
-                                "hello world")).eagerly().run {
-                    body should bePresent {
-                        it.asString(UTF_8) shouldBe "hello world"
-                    }
+            TcpRawHttpClient().send(RawHttp().parseRequest(
+                    "POST http://localhost:8083/echo HTTP/1.1\r\n" +
+                            "Host: localhost\r\n" +
+                            "Accept: text/plain\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "Content-Length: 11\r\n" +
+                            "\r\n" +
+                            "hello world")).eagerly().run {
+                body should bePresent {
+                    it.asString(UTF_8) shouldBe "hello world"
                 }
             }
         }
