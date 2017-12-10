d4b103a422d6b7b9c6decaa5dce3c177c592e4d3
renatoathaydes <renatoathaydes@hotmail.com>
2017-12-10 14:53:13 +0100

    Created StringBody.

---
+++ rawhttp-core/src/main/java/com/athaydes/rawhttp/core/body/StringBody.java
@@ -0,0 +1,59 @@
+package com.athaydes.rawhttp.core.body;
+
+import com.athaydes.rawhttp.core.BodyReader;
+import com.athaydes.rawhttp.core.LazyBodyReader;
+
+import javax.annotation.Nullable;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+public class StringBody extends HttpMessageBody {
+
+    private final byte[] body;
+    private final Charset charset;
+
+    @Nullable
+    private final String contentType;
+
+    public StringBody(String body) {
+        this(body, null);
+    }
+
+    public StringBody(String body,
+                      @Nullable String contentType) {
+        this(body, contentType, StandardCharsets.UTF_8);
+    }
+
+    public StringBody(String body,
+                      @Nullable String contentType,
+                      Charset charset) {
+        this.body = body.getBytes(charset);
+        this.contentType = contentType;
+        this.charset = charset;
+    }
+
+    @Override
+    public Optional<String> getContentType() {
+        return Optional.ofNullable(contentType);
+    }
+
+    @Override
+    public long getContentLength() {
+        return body.length;
+    }
+
+    public Charset getCharset() {
+        return charset;
+    }
+
+    @Override
+    public LazyBodyReader toBodyReader() {
+        return new LazyBodyReader(BodyReader.BodyType.CONTENT_LENGTH,
+                new ByteArrayInputStream(body),
+                (long) body.length,
+                false);
+    }
+
+}
