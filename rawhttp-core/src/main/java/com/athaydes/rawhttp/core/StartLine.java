ae5eaa2e16083d64004a737a0c13e0f5e57d870f
renatoathaydes <renatoathaydes@hotmail.com>
2018-02-05 20:58:30 +0100

    Javadocs for StartLine and RawHttpClient classes.

--- rawhttp-core/src/main/java/com/athaydes/rawhttp/core/StartLine.java
+++ rawhttp-core/src/main/java/com/athaydes/rawhttp/core/StartLine.java
@@ -1,5 +1,15 @@
 package com.athaydes.rawhttp.core;
 
+/**
+ * HTTP message start line.
+ * <p>
+ * See <a href="https://tools.ietf.org/html/rfc7230#section-3.1">Section 3.1</a> of RFC-7230.
+ */
 public interface StartLine {
+
+    /**
+     * @return message's HTTP version.
+     * Known values are {@code HTTP/1.0}, {@code HTTP/1.1} and {@code HTTP/2}.
+     */
     String getHttpVersion();
 }
