c32527b6e0c23fa6a8d8c5a8c478f55255429f6b
renatoathaydes <renatoathaydes@hotmail.com>
2018-02-13 21:49:16 +0100

    Created ChunkedBody to encode message body with "chunked".

---
+++ rawhttp-core/src/test/kotlin/com/athaydes/rawhttp/core/body/ChunkedBodyTest.kt
@@ -0,0 +1,66 @@
+package com.athaydes.rawhttp.core.body
+
+import com.athaydes.rawhttp.core.BodyReader
+import com.athaydes.rawhttp.core.bePresent
+import com.athaydes.rawhttp.core.shouldHaveSameElementsAs
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class ChunkedBodyTest : StringSpec({
+
+    "Can encode chunked body with single chunk" {
+        val stream = "Hi".byteInputStream()
+        val body = ChunkedBody(null, stream, 2)
+
+        body.toBodyReader().eager().should {
+            it.bodyType shouldBe BodyReader.BodyType.CHUNKED
+            it.asString(Charsets.UTF_8) shouldBe "Hi"
+            it.asChunkedBodyContents() should bePresent {
+                it.chunks.size shouldBe 2
+                it.chunks[0].data shouldHaveSameElementsAs "Hi".toByteArray()
+                it.chunks[1].data.size shouldBe 0 // last chunk
+                it.data shouldHaveSameElementsAs "Hi".toByteArray()
+                it.trailerHeaders.asMap().size shouldBe 0
+            }
+        }
+    }
+
+    "Can encode chunked body with single chunk (bigger than the data)" {
+        val stream = "Hi".byteInputStream()
+        val body = ChunkedBody(null, stream, 512)
+
+        body.toBodyReader().eager().should {
+            it.bodyType shouldBe BodyReader.BodyType.CHUNKED
+            it.asString(Charsets.UTF_8) shouldBe "Hi"
+            it.asChunkedBodyContents() should bePresent {
+                it.chunks.size shouldBe 2
+                it.chunks[0].data shouldHaveSameElementsAs "Hi".toByteArray()
+                it.chunks[1].data.size shouldBe 0 // last chunk
+                it.data shouldHaveSameElementsAs "Hi".toByteArray()
+                it.trailerHeaders.asMap().size shouldBe 0
+            }
+        }
+    }
+
+    "Can encode chunked body with several chunks" {
+        val stream = "Hello world".byteInputStream()
+        val body = ChunkedBody(null, stream, 4)
+
+        body.toBodyReader().eager().should {
+            it.bodyType shouldBe BodyReader.BodyType.CHUNKED
+            it.asString(Charsets.UTF_8) shouldBe "Hello world"
+            it.asChunkedBodyContents() should bePresent {
+                it.chunks.size shouldBe 4
+                it.chunks[0].data shouldHaveSameElementsAs "Hell".toByteArray()
+                it.chunks[1].data shouldHaveSameElementsAs "o wo".toByteArray()
+                it.chunks[2].data shouldHaveSameElementsAs "rld".toByteArray()
+                it.chunks[3].data.size shouldBe 0 // last chunk
+                it.data shouldHaveSameElementsAs "Hello world".toByteArray()
+                it.trailerHeaders.asMap().size shouldBe 0
+            }
+        }
+    }
+
+
+})
