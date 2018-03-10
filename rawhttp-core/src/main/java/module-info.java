29d7b63afcd55b921e6e53eed4503c0e2322bcae
renatoathaydes <renatoathaydes@hotmail.com>
2018-03-10 11:31:37 +0100

    Configured core module to compile with Java 9.

---
+++ rawhttp-core/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.athaydes.rawhttp.core {
+    requires com.athaydes.nullable;
+    exports com.athaydes.rawhttp.core;
+    exports com.athaydes.rawhttp.core.client;
+    exports com.athaydes.rawhttp.core.server;
+    exports com.athaydes.rawhttp.core.body;
+    exports com.athaydes.rawhttp.core.errors;
+}
