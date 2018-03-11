b226b959898e7f2f612f72dad92d9ca891b79fe6
renatoathaydes <renatoathaydes@hotmail.com>
2018-03-11 17:01:30 +0100

    Fixed Gradle setup.

--- rawhttp-core/src/main/java/module-info.java
+++ rawhttp-core/src/main/java/module-info.java
@@ -1,3 +1,13 @@
+/**
+ * Core RawHTTP module.
+ * <p>
+ * This module exposes all of the HTTP specification primitives, such as requests, responses and headers.
+ * <p>
+ * It also contains implementations of HTTP clients (see package {@link com.athaydes.rawhttp.core.client})
+ * and HTTP servers (see package {@link com.athaydes.rawhttp.core.server}).
+ * <p>
+ * The main entry-point to this library is the {@link com.athaydes.rawhttp.core.RawHttp} class.
+ */
 module com.athaydes.rawhttp.core {
     requires com.athaydes.nullable;
     exports com.athaydes.rawhttp.core;
