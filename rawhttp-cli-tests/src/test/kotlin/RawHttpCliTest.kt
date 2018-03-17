2afbb3b2f71260b3eb191c3683b7c54066318b6b
renatoathaydes <renatoathaydes@hotmail.com>
2018-03-17 22:24:18 +0100

    Created system tests for CLI.

---
+++ rawhttp-cli-tests/src/test/kotlin/RawHttpCliTest.kt
@@ -0,0 +1,137 @@
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.startsWith
+import org.junit.Assert.assertThat
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Test
+import java.io.File
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+
+class RawHttpCliTest : RawHttpCliTester() {
+
+
+    @Test
+    fun canPrintHelp() {
+        fun assertHelpOptOutput(handle: ProcessHandle) {
+            val exitValue = handle.waitForEndAndGetStatus()
+            assertThat(exitValue, equalTo(0))
+            assertThat(handle.err, equalTo(""))
+            assertThat(handle.out, startsWith("=============== RawHTTP CLI ==============="))
+        }
+
+        val shortOptHandle = runCli("-h")
+        assertHelpOptOutput(shortOptHandle)
+
+        val longOptHandle = runCli("--help")
+        assertHelpOptOutput(longOptHandle)
+    }
+
+    @Test
+    fun canReadRequestFromSysIn() {
+        val handle = runCli()
+
+        // write the request to the process sysin
+        handle.process.outputStream.writer().use {
+            it.write(SUCCESS_HTTP_REQUEST)
+        }
+
+        assertOutputIsSuccessResponse(handle)
+    }
+
+    @Test
+    fun canReadRequestFromArgument() {
+        val handle = runCli(SUCCESS_HTTP_REQUEST)
+        assertOutputIsSuccessResponse(handle)
+    }
+
+    @Test
+    fun canReadRequestFromFile() {
+        val tempFile = File.createTempFile(javaClass.name, "request")
+        tempFile.writeText(SUCCESS_HTTP_REQUEST)
+
+        val handle = runCli("-f", tempFile.absolutePath)
+        assertOutputIsSuccessResponse(handle)
+    }
+
+    @Test
+    fun canServeLocalDirectory() {
+        val workDir = File(".")
+        val someFileInWorkDir = workDir.listFiles()?.firstOrNull()
+                ?: return fail("Cannot run test, no files found in the working directory: ${workDir.absolutePath}")
+
+        val handle = runCli("-s")
+
+        val response = try {
+            sendHttpRequest("""
+            GET http://localhost:8080/${someFileInWorkDir.name}
+            Accept: */*
+            """.trimIndent()).eagerly()
+        } finally {
+            handle.sendStopSignalToRawHttpServer()
+        }
+
+        handle.verifyProcessTerminatedWithExitCode(0)
+
+        assertThat(response.statusCode, equalTo(200))
+        assertTrue(response.body.isPresent)
+        assertThat(response.body.get().asBytes(), equalTo(someFileInWorkDir.readBytes()))
+    }
+
+    @Test
+    fun canServeAnyDirectoryLoggingRequests() {
+        val tempDir = createTempDir(javaClass.name)
+        val someFile = File(tempDir, "my-file")
+        someFile.writeText("Hello RawHTTP!")
+
+        val handle = runCli("--log-requests", "--server", tempDir.absolutePath)
+
+        val response = try {
+            sendHttpRequest("""
+            GET http://localhost:8080/${someFile.name}
+            Accept: */*
+            """.trimIndent()).eagerly()
+        } finally {
+            handle.sendStopSignalToRawHttpServer()
+        }
+
+        handle.verifyProcessTerminatedWithExitCode(0)
+
+        assertThat("Server returned unexpected status code\n$handle",
+                response.statusCode, equalTo(200))
+        assertTrue(response.body.isPresent)
+        assertThat(response.body.get().asString(Charsets.UTF_8), equalTo("Hello RawHTTP!"))
+
+        // log format follows the Common Log Format - https://en.wikipedia.org/wiki/Common_Log_Format
+        val dateRegex = Regex("[0-9.:]+ \\[(?<date>.+)] \".+\" \\d{3} \\d+").toPattern()
+
+        // verify the request was logged
+        val lastOutputLine = handle.out.lines().asReversed().find { !it.isEmpty() }
+        val match = dateRegex.matcher(lastOutputLine)
+
+        if (!match.find()) {
+            return fail("Process output did not match the expected value:\n$handle")
+        }
+
+        val logDate = match.group("date")
+
+        // should be able to parse the date with the formatter used in Common Log Format
+        val dateFormat = DateTimeFormatter
+                .ofPattern("d/MMM/yyyy:HH:mm:ss z")
+                .withLocale(Locale.getDefault())
+                .withZone(ZoneId.systemDefault())
+
+        val parsedLogDate = LocalDateTime.parse(logDate, dateFormat)
+
+        // should be very recent date
+        assertTrue("Parsed Date seems too different from the expected: $parsedLogDate",
+                parsedLogDate.isBefore(LocalDateTime.now().plusSeconds(5)) &&
+                        parsedLogDate.isAfter(LocalDateTime.now().minusSeconds(10)))
+
+        assertThat(handle.err, equalTo(""))
+    }
+
+}
---
+++ rawhttp-cli-tests/src/test/kotlin/RawHttpCliTester.kt
@@ -0,0 +1,186 @@
+import com.athaydes.rawhttp.core.EagerHttpResponse
+import com.athaydes.rawhttp.core.RawHttp
+import com.athaydes.rawhttp.core.RawHttpResponse
+import com.athaydes.rawhttp.core.client.TcpRawHttpClient
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.AfterClass
+import org.junit.Assert.assertThat
+import org.junit.Assert.fail
+import org.junit.BeforeClass
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.lang.Thread.sleep
+import java.net.ServerSocket
+import java.util.concurrent.TimeUnit
+
+data class ProcessHandle(val process: Process,
+                         private val outputStream: ByteArrayOutputStream,
+                         private val errStream: ByteArrayOutputStream) {
+
+    fun waitForEndAndGetStatus(): Int {
+        val completed = process.waitFor(2, TimeUnit.SECONDS)
+
+        if (!completed) {
+            process.destroyForcibly()
+            fail("Process not completed within the timeout:\n$this")
+        }
+        return process.exitValue()
+    }
+
+    val out: String by lazy(LazyThreadSafetyMode.NONE) {
+        waitForEndAndGetStatus()
+        outputStream.toString()
+    }
+
+    val err: String by lazy(LazyThreadSafetyMode.NONE) {
+        waitForEndAndGetStatus()
+        errStream.toString()
+    }
+
+}
+
+abstract class RawHttpCliTester {
+
+    companion object {
+        val CLI_EXECUTABLE: Array<String>
+
+        val SUCCESS_HTTP_REQUEST = "GET /saysomething HTTP/1.1\r\n" +
+                "Host: localhost:8083\r\n" +
+                "Accept: */*\r\n" +
+                "User-Agent: RawHTTP"
+
+        val SUCCESS_HTTP_RESPONSE = "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: 9\r\n" +
+                "\r\n" +
+                "something"
+
+        val NOT_FOUND_HTTP_RESPONSE = "HTTP/1.1 404 Not Found\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: 18\r\n" +
+                "\r\n" +
+                "Resource Not Found".trimIndent()
+
+        var httpServerThread: Thread? = null
+
+        init {
+            val rootDir = System.getProperty("RAWHTTP_CLI_ROOT_DIR")
+                    ?: throw IllegalStateException("RAWHTTP_CLI_ROOT_DIR system property must be set")
+
+            val launcher = File(rootDir, "dist/bin/java")
+            if (!launcher.isFile) {
+                throw IllegalStateException("The CLI launcher does not exist: $launcher")
+            }
+
+            CLI_EXECUTABLE = arrayOf(launcher.absolutePath,
+                    "-m", "com.athaydes.rawhttp.cli/com.athaydes.rawhttp.cli.Main")
+        }
+
+        @BeforeClass
+        @JvmStatic
+        fun startHttpServer() {
+            val http = RawHttp()
+            val server = ServerSocket(8083)
+
+            httpServerThread = Thread {
+                try {
+                    while (true) {
+                        val client = server.accept()
+                        println("Accepted connection from client $client")
+                        try {
+                            val request = http.parseRequest(client.getInputStream())
+
+                            println("Received Request:\n$request")
+
+                            if (request.uri.path == "/saysomething") {
+                                http.parseResponse(SUCCESS_HTTP_RESPONSE)
+                                        .writeTo(client.getOutputStream())
+                            } else {
+                                http.parseResponse(NOT_FOUND_HTTP_RESPONSE).writeTo(client.getOutputStream())
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        } finally {
+                            client.close()
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    println("RawHttpCliTest HTTP Server stopped")
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.apply { start() }
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun stopHttpServer() {
+            httpServerThread!!.interrupt()
+        }
+
+        fun runCli(vararg args: String): ProcessHandle {
+            val process = ProcessBuilder().command(*CLI_EXECUTABLE, *args).start()
+            val outputStream = ByteArrayOutputStream(1024)
+            val errStream = ByteArrayOutputStream(1024)
+
+            Thread {
+                process.inputStream.transferTo(outputStream)
+            }.start()
+            Thread {
+                process.errorStream.transferTo(errStream)
+            }.start()
+
+            return ProcessHandle(process, outputStream, errStream)
+        }
+
+        fun assertOutputIsSuccessResponse(handle: ProcessHandle) {
+            handle.verifyProcessTerminatedWithExitCode(0)
+            assertThat(handle.out, equalTo(SUCCESS_HTTP_RESPONSE))
+            assertThat(handle.err, equalTo(""))
+        }
+
+        fun sendHttpRequest(request: String): RawHttpResponse<*> {
+            var response: EagerHttpResponse<*>? = null
+            var failedConnectionAttempts = 0
+            while (failedConnectionAttempts < 10) {
+                try {
+                    val client = TcpRawHttpClient()
+                    response = client.send(RawHttp().parseRequest(request)).eagerly()
+                    client.close()
+                    break
+                } catch (e: IOException) {
+                    failedConnectionAttempts++
+                    Thread.sleep(100)
+                }
+            }
+
+            return response
+                    ?: throw AssertionError("Unable to connect to server after $failedConnectionAttempts failed attempts")
+        }
+
+        fun ProcessHandle.verifyProcessTerminatedWithExitCode(expectedExitCode: Int) {
+            val statusCode = waitForEndAndGetStatus()
+            if (statusCode != expectedExitCode) {
+                println("Process sysout:\n$out")
+                println("Process syserr:\n$err")
+                throw AssertionError("Expected process to exit with code $expectedExitCode but was $statusCode")
+            }
+        }
+
+        fun ProcessHandle.sendStopSignalToRawHttpServer() {
+            // the tests expect all output to be flush from the process,
+            // so we need to wait a little bit before killing the server otherwise
+            // process output may be missing
+            sleep(250L)
+            try {
+                process.outputStream.write('\n'.toInt())
+                process.outputStream.flush()
+            } catch (e: IOException) {
+                // server probably died early
+            }
+        }
+
+    }
+
+}
