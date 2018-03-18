9dc63838962747c74053851e62ecb466d24a6d38
renatoathaydes <renatoathaydes@hotmail.com>
2018-03-18 17:35:01 +0100

    CLI Server - provide a few content-types based on resource name.

--- rawhttp-cli/src/main/java/com/athaydes/rawhttp/cli/CliServerRouter.java
+++ rawhttp-cli/src/main/java/com/athaydes/rawhttp/cli/CliServerRouter.java
@@ -12,10 +12,34 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static java.util.Map.entry;
+
 final class CliServerRouter implements Router {
+
+    private static final Map<String, String> mimeMapping;
+
+    static {
+        mimeMapping = Map.ofEntries(
+                entry("html", "text/html"),
+                entry("txt", "text/plain"),
+                entry("json", "application/json"),
+                entry("js", "application/javascript"),
+                entry("xml", "application/xml"),
+                entry("jpg", "image/jpeg"),
+                entry("jpeg", "image/jpeg"),
+                entry("gif", "image/gif"),
+                entry("png", "image/png"),
+                entry("tif", "image/tiff"),
+                entry("tiff", "image/tiff"),
+                entry("ico", "image/x-icon"),
+                entry("pdf", "application/pdf"),
+                entry("css", "text/css")
+        );
+    }
 
     private static final RawHttp http = new RawHttp();
 
@@ -38,7 +62,7 @@ final class CliServerRouter implements Router {
             if (resource.isFile()) {
                 response = http.parseResponse(request.getStartLine().getHttpVersion() +
                         " 200 OK\n" +
-                        "Content-Type: application/octet-stream\n" +
+                        "Content-Type: " + mimeTypeOf(resource.getName()) + "\n" +
                         "Server: RawHTTP")
                         .replaceBody(new FileBody(resource));
             } else {
@@ -59,6 +83,15 @@ final class CliServerRouter implements Router {
         }
         requestLogger.logRequest(request, response);
         return response;
+    }
+
+    static String mimeTypeOf(String resourceName) {
+        int idx = resourceName.lastIndexOf('.');
+        if (idx < 0 || idx == resourceName.length() - 1) {
+            return "application/octet-stream";
+        }
+        String extension = resourceName.substring(idx + 1);
+        return mimeMapping.getOrDefault(extension, "application/octet-stream");
     }
 
 }
