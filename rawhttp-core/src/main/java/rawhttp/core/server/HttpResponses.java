e8c9f6e160290ae28cdf703272810492651577b2
renatoathaydes <renatoathaydes@hotmail.com>
2018-05-16 22:11:10 +0200

    Improved building of pre-built HTTP responses.

--- rawhttp-core/src/main/java/rawhttp/core/server/HttpResponses.java
+++ rawhttp-core/src/main/java/rawhttp/core/server/HttpResponses.java
@@ -1,12 +1,12 @@
 package rawhttp.core.server;
 
-import java.io.IOException;
 import rawhttp.core.EagerHttpResponse;
-import rawhttp.core.HttpMetadataParser;
 import rawhttp.core.HttpVersion;
+import rawhttp.core.RawHttpHeaders;
 import rawhttp.core.StatusLine;
+import rawhttp.core.body.EagerBodyReader;
 
-import static rawhttp.core.server.TcpRawHttpServer.STRICT_HTTP;
+import static java.nio.charset.StandardCharsets.US_ASCII;
 
 final class HttpResponses {
 
@@ -21,42 +21,40 @@ final class HttpResponses {
     private static final StatusLine STATUS_500_HTTP1_1;
 
     static {
-        HttpMetadataParser metadataParser = STRICT_HTTP.getMetadataParser();
+        STATUS_404_HTTP1_0 = new StatusLine(HttpVersion.HTTP_1_0, 404, "Not Found");
+        STATUS_404_HTTP1_1 = new StatusLine(HttpVersion.HTTP_1_1, 404, "Not Found");
 
-        STATUS_404_HTTP1_0 = metadataParser.parseStatusLine(
-                "HTTP/1.0 404 Not Found");
-        STATUS_404_HTTP1_1 = metadataParser.parseStatusLine(
-                "HTTP/1.1 404 Not Found");
-        STATUS_500_HTTP1_0 = metadataParser.parseStatusLine(
-                "HTTP/1.0 500 Server Error");
-        STATUS_500_HTTP1_1 = metadataParser.parseStatusLine(
-                "HTTP/1.1 500 Server Error");
+        STATUS_500_HTTP1_0 = new StatusLine(HttpVersion.HTTP_1_0, 500, "Server Error");
+        STATUS_500_HTTP1_1 = new StatusLine(HttpVersion.HTTP_1_1, 500, "Server Error");
 
-        try {
-            NOT_FOUND_404_HTTP1_1 = STRICT_HTTP.parseResponse(
-                    STATUS_404_HTTP1_1 + "\r\n" +
-                            "Content-Type: text/plain\r\n" +
-                            "Content-Length: 23\r\n" +
-                            "Cache-Control: no-cache\r\n" +
-                            "Pragma: no-cache\r\n" +
-                            "\r\n" +
-                            "Resource was not found.").eagerly();
+        final RawHttpHeaders basicHeaders = RawHttpHeaders.newBuilderSkippingValidation()
+                .with("Content-Type", "text/plain")
+                .with("Cache-Control", "no-cache")
+                .with("Pragma", "no-cache")
+                .build();
 
-            NOT_FOUND_404_HTTP1_0 = NOT_FOUND_404_HTTP1_1.withStatusLine(STATUS_404_HTTP1_0);
+        byte[] notFoundResponseBody = "Resource was not found.".getBytes(US_ASCII);
 
-            SERVER_ERROR_500_HTTP1_1 = STRICT_HTTP.parseResponse(
-                    STATUS_500_HTTP1_1 + "\r\n" +
-                            "Content-Type: text/plain\r\n" +
-                            "Content-Length: 28\r\n" +
-                            "Cache-Control: no-cache\r\n" +
-                            "Pragma: no-cache\r\n" +
-                            "\r\n" +
-                            "A Server Error has occurred.").eagerly();
+        NOT_FOUND_404_HTTP1_1 = new EagerHttpResponse<>(null, null,
+                STATUS_404_HTTP1_1,
+                RawHttpHeaders.newBuilderSkippingValidation(basicHeaders)
+                        .overwrite("Content-Length", Integer.toString(notFoundResponseBody.length))
+                        .build(),
+                new EagerBodyReader(notFoundResponseBody));
 
-            SERVER_ERROR_500_HTTP1_0 = SERVER_ERROR_500_HTTP1_1.withStatusLine(STATUS_500_HTTP1_0);
-        } catch (IOException e) {
-            throw new IllegalStateException("Default HTTP responses could not be parsed");
-        }
+        NOT_FOUND_404_HTTP1_0 = NOT_FOUND_404_HTTP1_1.withStatusLine(STATUS_404_HTTP1_0);
+
+        byte[] serverErrorResponseBody = "A Server Error has occurred.".getBytes(US_ASCII);
+
+        SERVER_ERROR_500_HTTP1_1 = new EagerHttpResponse<>(null, null,
+                STATUS_500_HTTP1_1,
+                RawHttpHeaders.newBuilderSkippingValidation()
+                        .overwrite("Content-Length", Integer.toString(serverErrorResponseBody.length))
+                        .build(),
+                new EagerBodyReader(serverErrorResponseBody));
+
+        SERVER_ERROR_500_HTTP1_0 = SERVER_ERROR_500_HTTP1_1.withStatusLine(STATUS_500_HTTP1_0);
+
     }
 
     static EagerHttpResponse<Void> getNotFoundResponse(HttpVersion httpVersion) {
