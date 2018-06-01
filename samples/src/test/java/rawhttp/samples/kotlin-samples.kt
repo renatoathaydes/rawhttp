fc93902f9ead0eb353dccf12429b8e5ef8ded7c3
renatoathaydes <renatoathaydes@hotmail.com>
2018-06-01 20:02:44 +0200

    Stop using StringSpec in samples tests.

--- samples/src/test/java/rawhttp/samples/kotlin-samples.kt
+++ samples/src/test/java/rawhttp/samples/kotlin-samples.kt
@@ -1,19 +1,20 @@
 package rawhttp.samples
 
-import io.kotlintest.Spec
-import io.kotlintest.specs.StringSpec
 import org.hamcrest.CoreMatchers.equalTo
+import org.junit.AfterClass
 import org.junit.Assert.assertThat
 import org.junit.Assert.assertTrue
+import org.junit.BeforeClass
+import org.junit.Test
 import rawhttp.core.RawHttp
 import rawhttp.core.client.TcpRawHttpClient
 import spark.Spark
+import java.lang.Thread.sleep
 import java.net.Socket
 import java.net.URI
 import kotlin.text.Charsets.UTF_8
 
-@Suppress("UNUSED_PARAMETER", "UNUSED_ANONYMOUS_PARAMETER")
-fun sparkServerInterceptor(spec: Spec, runTest: () -> Unit) {
+fun startSparkServer() {
     Spark.port(8082)
     Spark.get("/hello", "text/plain") { req, res -> "Hello" }
     Spark.post("/repeat", "text/plain") { req, res ->
@@ -21,64 +22,77 @@ fun sparkServerInterceptor(spec: Spec, runTest: () -> Unit) {
         val text = req.queryParamOrDefault("text", "x")
         text.repeat(count)
     }
-    Thread.sleep(150L)
-    runTest()
+    sleep(150L)
+}
+
+fun stopSparkServer() {
     Spark.stop()
 }
 
-class KotlinSamples : StringSpec() {
+class KotlinSamples {
 
-    override val specInterceptors: List<(Spec, () -> Unit) -> Unit>
-        get() = listOf(::sparkServerInterceptor)
-
-    init {
-        "Simple RawHTTP Kotlin Sample" {
-            val req = RawHttp().parseRequest("GET /hello\r\nHost: localhost:8082").eagerly().run {
-                assertThat(method, equalTo("GET"))
-                assertThat(uri, equalTo(URI.create("http://localhost:8082/hello")))
-                assertThat(headers["Host"], equalTo(listOf("localhost")))
-                this
-            }
-
-            // send request to the Spark server
-            val socket = Socket("localhost", 8082)
-            req.writeTo(socket.getOutputStream())
-
-            // check the response
-            RawHttp().parseResponse(socket.getInputStream()).run {
-                assertThat(statusCode, equalTo(200))
-                assertTrue(body.isPresent)
-                if (body.get().isChunked) {
-                    assertThat(body.get().asChunkedBodyContents().get().asString(UTF_8), equalTo("Hello"))
-                } else {
-                    assertThat(body.get().asString(UTF_8), equalTo("Hello"))
-                }
-            }
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            startSparkServer()
+            sleep(250L)
         }
 
-        "Post something using the TCP client" {
-            val client = TcpRawHttpClient()
+        @AfterClass
+        @JvmStatic
+        fun cleanup() {
+            stopSparkServer()
+        }
+    }
 
-            val req = RawHttp().parseRequest("""
+    @Test
+    fun simpleRawHTTPKotlinSample() {
+        val req = RawHttp().parseRequest("GET /hello\r\nHost: localhost:8082").eagerly().run {
+            assertThat(method, equalTo("GET"))
+            assertThat(uri, equalTo(URI.create("http://localhost:8082/hello")))
+            assertThat(headers["Host"], equalTo(listOf("localhost")))
+            this
+        }
+
+        // send request to the Spark server
+        val socket = Socket("localhost", 8082)
+        req.writeTo(socket.getOutputStream())
+
+        // check the response
+        RawHttp().parseResponse(socket.getInputStream()).run {
+            assertThat(statusCode, equalTo(200))
+            assertTrue(body.isPresent)
+            if (body.get().isChunked) {
+                assertThat(body.get().asChunkedBodyContents().get().asString(UTF_8), equalTo("Hello"))
+            } else {
+                assertThat(body.get().asString(UTF_8), equalTo("Hello"))
+            }
+        }
+    }
+
+    @Test
+    fun postSomethingUsingTheTCPClient() {
+        val client = TcpRawHttpClient()
+
+        val req = RawHttp().parseRequest("""
                 POST /repeat?text=helloKotlin&count=1000
                 User-Agent: RawHTTP
                 Host: localhost:8082""".trimIndent())
 
-            val res = client.send(req)
+        val res = client.send(req)
 
-            res.run {
-                assertThat(statusCode, equalTo(200))
-                assertTrue(body.isPresent)
+        res.run {
+            assertThat(statusCode, equalTo(200))
+            assertTrue(body.isPresent)
 
-                val expectedBody = "helloKotlin".repeat(1_000)
+            val expectedBody = "helloKotlin".repeat(1_000)
 
-                if (body.get().isChunked) {
-                    assertThat(body.get().asChunkedBodyContents().get().asString(UTF_8), equalTo(expectedBody))
-                } else {
-                    assertThat(body.get().asString(UTF_8), equalTo(expectedBody))
-                }
+            if (body.get().isChunked) {
+                assertThat(body.get().asChunkedBodyContents().get().asString(UTF_8), equalTo(expectedBody))
+            } else {
+                assertThat(body.get().asString(UTF_8), equalTo(expectedBody))
             }
-
         }
     }
 
