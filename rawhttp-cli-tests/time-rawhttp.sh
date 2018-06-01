05a5a575bad6c3cbbb208957098b221fe4b3b740
renatoathaydes <renatoathaydes@hotmail.com>
2018-06-01 18:44:58 +0200

    Improvements to server-startup timing script.

--- rawhttp-cli-tests/time-rawhttp.sh
+++ rawhttp-cli-tests/time-rawhttp.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 # command that tests that the server is accepting connections
-CMD="curl localhost:8082 >& /dev/null"
+CMD="curl localhost:8081 >& /dev/null"
+
+
+START_TIME=$(gdate +%s%3N)
 
 # start the server
 ../rawhttp serve . -p 8082 &
 SERVER_PID=$!
 
-
 STEP=0.001      # sleep between tries, in seconds
-MAX_TRIES=500
-TRIES_LEFT=${MAX_TRIES}
+TRIES=500
 eval ${CMD}
 while [[ $? -ne 0 ]]; do
-  ((TRIES_LEFT--))
-  echo -ne "Tries left: $TRIES_LEFT"\\r
-  if [[ TRIES_LEFT -eq 0 ]]; then
+  ((TRIES--))
+  echo -ne "Tries left: $TRIES"\\r
+  if [[ TRIES -eq 0 ]]; then
     echo "Server not started within timeout"
     exit 1
   fi
   sleep ${STEP}
   eval ${CMD}
 done
-if [[ ${TRIES_LEFT} -gt 0 ]]; then
-  TIME=$(echo "$STEP * ($MAX_TRIES - $TRIES_LEFT)" | bc)
-  echo "Server connected in $TIME seconds"
-  #./rawhttp send -t "GET localhost:8082/hello"
-fi
+
+END_TIME=$(gdate +%s%3N)
+TIME=$(($END_TIME - $START_TIME))
+echo "Server connected in $TIME ms"
+#./rawhttp send -t "GET localhost:8082/hello"
 
 kill ${SERVER_PID}
