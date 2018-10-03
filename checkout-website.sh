cbbb4b324e67766422c3be8bb0732b5740b65f55
renato <renato.athaydes@curity.io>
2018-10-03 20:08:58 +0200

    Added bash scripts for website management.

---
+++ checkout-website.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+cd $DIR/
+
+echo "Now in directory $(pwd)"
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Cleaning site directory"
+rm -rf site
+mkdir site
+git worktree prune
+rm -rf .git/worktrees/site/
+
+echo "Checking out site branch into site/"
+git worktree add -B site site origin/site
+
+echo "Done!"
