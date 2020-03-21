7ce27201d9d21388864ea0e520c87ebd9876dbd3
renatoathaydes <renatoathaydes@hotmail.com>
2020-03-21 17:55:50 +0100

    Core: test for too-many-redirects.

--- rawhttp-core/src/test/kotlin/rawhttp/core/client/RedirectingRawHttpClientTest.kt
+++ rawhttp-core/src/test/kotlin/rawhttp/core/client/RedirectingRawHttpClientTest.kt
@@ -1,6 +1,7 @@
 package rawhttp.core.client
 
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldThrow
 import org.junit.Test
 import rawhttp.core.RawHttp
 import kotlin.test.fail
@@ -54,6 +55,31 @@ class RedirectingRawHttpClientTest {
                 http.parseRequest("GET /\nHost: myhost"))
 
         actualResponse shouldBe resource
+    }
+
+    @Test
+    fun errorOnTooManyRedirects() {
+        val redirect1 = http.parseResponse("302 Found\nLocation: /foo").eagerly()
+        val redirect2 = http.parseResponse("302 Found\nLocation: bar").eagerly() // relative to /foo
+        val redirect3 = http.parseResponse("302 Found\nLocation: /foo/bar/foo").eagerly()
+
+        val resource = http.parseResponse("200 OK").eagerly()
+
+        val mockClient = RawHttpClient { req ->
+            when (req.uri.path) {
+                "/" -> redirect1
+                "/foo" -> redirect2
+                "/foo/bar" -> redirect3
+                "/foo/bar/foo" -> resource
+                else -> fail("unexpected request: $req")
+            }
+        }
+
+        val redirectingClient = RedirectingRawHttpClient(mockClient, 2)
+
+        shouldThrow<IllegalStateException> {
+            redirectingClient.send(http.parseRequest("GET /\nHost: myhost"))
+        }.localizedMessage shouldBe "Too many redirects"
     }
 
 }
