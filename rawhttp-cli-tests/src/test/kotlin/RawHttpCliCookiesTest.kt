627b2eac9118ec866bfe59534421f3a601dd96ca
renatoathaydes <renatoathaydes@hotmail.com>
2020-03-22 12:03:48 +0100

    CLI: Completed CLI tests for redirects/cookies.

--- rawhttp-cli-tests/src/test/kotlin/RawHttpCliCookiesTest.kt
+++ rawhttp-cli-tests/src/test/kotlin/RawHttpCliCookiesTest.kt
@@ -1,4 +1,5 @@
 import RawHttpCliTester.Companion.verifyProcessTerminatedWithExitCode
+import io.kotlintest.matchers.haveSize
 import io.kotlintest.matchers.match
 import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
@@ -132,29 +133,39 @@ class RawHttpCliCookiesTest {
 
         val outLines = handle.out.lines()
 
-        outLines[0] shouldBe "HTTP/1.1 302"
-        outLines[1] shouldBe "Location: http://localhost:8084/login"
-        outLines[2] shouldBe "Content-Length: 0"
-        outLines[3] shouldBe ""
-        outLines[4] shouldBe "HTTP/1.1 200 OK"
-        outLines[5] shouldBe "Content-Type: text/plain"
-        outLines[6] shouldBe "Content-Length: 28"
-        outLines[7] shouldBe ""
-        outLines[8] shouldBe "Send your credentials to me!"
-        outLines[9] should match("TEST OK \\(\\d+ms\\): We are automatically redirected to the login page")
-        outLines[10] shouldBe "HTTP/1.1 401 Bad Credentials"
-        outLines[11] shouldBe "Content-Length: 0"
-        outLines[12] shouldBe ""
-        outLines[13] shouldBe ""
-        outLines[14] should match("TEST OK \\(\\d+ms\\): We get the bad credentials response")
-        outLines[15] shouldBe "HTTP/1.1 200 OK"
-        outLines[16] shouldBe "Content-Type: text/plain"
-        outLines[17] shouldBe "Content-Length: 9"
-        outLines[18] shouldBe "Set-Cookie: sid=\"foo\""
-        outLines[19] shouldBe ""
-        outLines[20] shouldBe "something"
-        // TODO more output to check
+        outLines should haveSize(29)
 
+        outLines.subList(0, 9) shouldBe listOf("HTTP/1.1 302",
+                "Location: http://localhost:8084/login",
+                "Content-Length: 0",
+                "",
+                "HTTP/1.1 200 OK",
+                "Content-Type: text/plain",
+                "Content-Length: 28",
+                "",
+                "Send your credentials to me!")
+        outLines[9] should match("TEST OK \\(\\d+ms\\): We are automatically redirected to the login page")
+        outLines.subList(10, 14) shouldBe listOf("HTTP/1.1 401 Bad Credentials",
+                "Content-Length: 0",
+                "",
+                "")
+        outLines[14] should match("TEST OK \\(\\d+ms\\): We get the bad credentials response")
+        outLines.subList(15, 21) shouldBe listOf("HTTP/1.1 200 OK",
+                "Content-Type: text/plain",
+                "Content-Length: 9",
+                "Set-Cookie: sid=\"foo\"",
+                "",
+                "something")
+        outLines[21] should match("TEST OK \\(\\d+ms\\): We get the SID cookie")
+        outLines.subList(22, 27) shouldBe listOf("HTTP/1.1 200 OK",
+                "Content-Type: text/plain",
+                "Content-Length: 10",
+                "",
+                "Hello user")
+        outLines[27] should match("TEST OK \\(\\d+ms\\): With the login cookie, we can get what we wanted")
+        outLines[28] shouldBe ""
+
+        handle.err shouldBe ""
     }
 
 }
