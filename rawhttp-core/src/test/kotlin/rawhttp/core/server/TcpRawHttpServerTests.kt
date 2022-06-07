8b47fdf8a8c159fec6d00a2afa7e000324aa9511
Renato Athaydes <renato@athaydes.com>
2022-06-07 18:41:15 +0200

    Re-wrote one more test in plain JUnit.

--- rawhttp-core/src/test/kotlin/rawhttp/core/server/TcpRawHttpServerTests.kt
+++ rawhttp-core/src/test/kotlin/rawhttp/core/server/TcpRawHttpServerTests.kt
@@ -1,12 +1,13 @@
 package rawhttp.core.server
 
 import io.kotest.assertions.fail
-import io.kotest.core.spec.Spec
-import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.optional.beEmpty
 import io.kotest.matchers.optional.shouldBePresent
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldHave
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import rawhttp.core.RawHttp
 import rawhttp.core.RawHttp.waitForPortToBeTaken
 import rawhttp.core.RawHttpHeaders
@@ -25,14 +26,35 @@ import java.nio.charset.StandardCharsets
 import java.time.Duration
 import java.util.Optional
 
-class TcpRawHttpServerTests : StringSpec() {
+class TcpRawHttpServerTests {
 
     companion object {
         private val http = RawHttp()
-    }
+        private val server = TcpRawHttpServer(8093)
+        private val httpClient = TcpRawHttpClient()
 
-    private val server = TcpRawHttpServer(8093)
-    private val httpClient = TcpRawHttpClient()
+        @JvmStatic
+        @BeforeAll
+        fun beforeSpec() {
+            startServer()
+            waitForPortToBeTaken(8093, Duration.ofSeconds(2))
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun afterSpec() {
+            cleanup()
+        }
+
+        private fun startServer() {
+            server.start(TestRouter)
+        }
+
+        private fun cleanup() {
+            server.stop()
+            httpClient.close()
+        }
+    }
 
     private object TestRouter : Router {
         override fun route(req: RawHttpRequest): Optional<RawHttpResponse<*>> {
@@ -79,208 +101,200 @@ class TcpRawHttpServerTests : StringSpec() {
         }
     }
 
-    private fun startServer() {
-        server.start(TestRouter)
+    @Test
+    fun `Server can handle successful http client request`() {
+        val request = http.parseRequest("GET http://localhost:8093/hello")
+        val response = httpClient.send(request).eagerly()
+
+        response.statusCode shouldBe 200
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "Hello RawHTTP!"
+        }
     }
 
-    private fun cleanup() {
-        server.stop()
-        httpClient.close()
+    @Test
+    fun `Server can handle multiple successful http client requests, including HEAD request`() {
+        val getRequest = http.parseRequest("GET http://localhost:8093/hello")
+        val headRequest = http.parseRequest("HEAD http://localhost:8093/hello")
+        val response = httpClient.send(getRequest).eagerly()
+
+        response.statusCode shouldBe 200
+        response.headers["Content-Length"] shouldBe listOf("14")
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "Hello RawHTTP!"
+        }
+
+        sleep(500)
+
+        val response2 = httpClient.send(headRequest).eagerly()
+
+        response2.statusCode shouldBe 200
+        response2.headers["Content-Length"] shouldBe listOf("14")
+        response2.body shouldBe beEmpty<EagerBodyReader>()
+
+        val response3 = httpClient.send(getRequest).eagerly()
+
+        response3.statusCode shouldBe 200
+        response3.headers["Content-Length"] shouldBe listOf("14")
+        response3.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "Hello RawHTTP!"
+        }
     }
 
-    override fun beforeSpec(spec: Spec) {
-        startServer()
-        waitForPortToBeTaken(8093, Duration.ofSeconds(2))
+    @Test
+    fun `Server can handle wrong method http client request`() {
+        val request = http.parseRequest("PUT http://localhost:8093")
+        val response = httpClient.send(request).eagerly()
+
+        response.statusCode shouldBe 405
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "Sorry, can't handle this method"
+        }
     }
 
-    override fun afterSpec(spec: Spec) {
-        cleanup()
+    @Test
+    fun `Server can handle wrong path http client request`() {
+        val request = http.parseRequest("GET http://localhost:8093/wrong/path")
+        val response = httpClient.send(request).eagerly()
+
+        response.statusCode shouldBe 404
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "Content was not found"
+        }
     }
 
-    init {
-        "Server can handle successful http client request" {
-            val request = http.parseRequest("GET http://localhost:8093/hello")
-            val response = httpClient.send(request).eagerly()
+    @Test
+    fun `Server returns default error response when router throws an Exception`() {
+        val request = http.parseRequest("POST http://localhost:8093/throw")
+        val response = httpClient.send(request).eagerly()
 
-            response.statusCode shouldBe 200
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "Hello RawHTTP!"
-            }
+        response.statusCode shouldBe 500
+        response.headers shouldHave validDateHeader()
+        response.headers["Content-Type"] shouldBe listOf("text/plain")
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "A Server Error has occurred."
         }
+    }
 
-        "Server can handle multiple successful http client requests, including HEAD request" {
-            val getRequest = http.parseRequest("GET http://localhost:8093/hello")
-            val headRequest = http.parseRequest("HEAD http://localhost:8093/hello")
-            val response = httpClient.send(getRequest).eagerly()
+    @Test
+    fun `Server returns default error response when request handler throws an Exception`() {
+        val request = http.parseRequest("DELETE http://localhost:8093")
+        val response = httpClient.send(request).eagerly()
 
-            response.statusCode shouldBe 200
-            response.headers["Content-Length"] shouldBe listOf("14")
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "Hello RawHTTP!"
-            }
-
-            sleep(500)
-
-            val response2 = httpClient.send(headRequest).eagerly()
-
-            response2.statusCode shouldBe 200
-            response2.headers["Content-Length"] shouldBe listOf("14")
-            response2.body shouldBe beEmpty<EagerBodyReader>()
-
-            val response3 = httpClient.send(getRequest).eagerly()
-
-            response3.statusCode shouldBe 200
-            response3.headers["Content-Length"] shouldBe listOf("14")
-            response3.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "Hello RawHTTP!"
-            }
+        response.statusCode shouldBe 500
+        response.headers shouldHave validDateHeader()
+        response.headers["Content-Type"] shouldBe listOf("text/plain")
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "A Server Error has occurred."
         }
+    }
 
-        "Server can handle wrong method http client request" {
-            val request = http.parseRequest("PUT http://localhost:8093")
-            val response = httpClient.send(request).eagerly()
+    @Test
+    fun `Server returns default error response when request handler returns null`() {
+        val request = http.parseRequest("Get http://localhost:8093/null")
+        val response = httpClient.send(request).eagerly()
 
-            response.statusCode shouldBe 405
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "Sorry, can't handle this method"
-            }
+        response.statusCode shouldBe 404
+        response.headers shouldHave validDateHeader()
+        response.headers["Content-Type"] shouldBe listOf("text/plain")
+        response.body shouldBePresent {
+            it.asRawString(Charsets.UTF_8) shouldBe "Resource was not found."
         }
+    }
 
-        "Server can handle wrong path http client request" {
-            val request = http.parseRequest("GET http://localhost:8093/wrong/path")
-            val response = httpClient.send(request).eagerly()
+    @Test
+    fun `Server should persist connection on all HTTP1_1 requests`() {
+        val socket = Socket("0.0.0.0", 8093)
+        http.parseRequest("GET /hello HTTP/1.1\r\nHost: localhost").writeTo(socket.getOutputStream())
+        val response = http.parseResponse(socket.getInputStream()).eagerly(true)
 
-            response.statusCode shouldBe 404
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "Content was not found"
-            }
-        }
+        response.statusCode shouldBe 200
 
-        "Server returns default error response when router throws an Exception" {
-            val request = http.parseRequest("POST http://localhost:8093/throw")
-            val response = httpClient.send(request).eagerly()
+        // the server should persist the connection
+        socket.assertIsOpen()
+    }
 
-            response.statusCode shouldBe 500
-            response.headers shouldHave validDateHeader()
-            response.headers["Content-Type"] shouldBe listOf("text/plain")
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "A Server Error has occurred."
-            }
-        }
+    @Test
+    fun `Server should close connection on HTTP1_1 requests if 'Connection close' header is sent`() {
+        val socket = Socket("0.0.0.0", 8093)
+        http.parseRequest(
+            "GET /hello HTTP/1.1\r\n" +
+                    "Host: localhost\r\n" +
+                    "Connection: close"
+        ).writeTo(socket.getOutputStream())
+        val response = http.parseResponse(socket.getInputStream()).eagerly(true)
 
-        "Server returns default error response when request handler throws an Exception" {
-            val request = http.parseRequest("DELETE http://localhost:8093")
-            val response = httpClient.send(request).eagerly()
+        response.statusCode shouldBe 200
 
-            response.statusCode shouldBe 500
-            response.headers shouldHave validDateHeader()
-            response.headers["Content-Type"] shouldBe listOf("text/plain")
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "A Server Error has occurred."
-            }
-        }
+        // the server should have closed this Socket
+        socket.assertIsClosed()
+    }
 
-        "Server returns default error response when request handler returns null" {
-            val request = http.parseRequest("Get http://localhost:8093/null")
-            val response = httpClient.send(request).eagerly()
+    @Test
+    fun `Server should close connection on HTTP1_0 requests`() {
+        val socket = Socket("0.0.0.0", 8093)
+        http.parseRequest("GET /hello HTTP/1.0\r\nHost: localhost").writeTo(socket.getOutputStream())
+        val response = http.parseResponse(socket.getInputStream()).eagerly(true)
 
-            response.statusCode shouldBe 404
-            response.headers shouldHave validDateHeader()
-            response.headers["Content-Type"] shouldBe listOf("text/plain")
-            response.body shouldBePresent {
-                it.asRawString(Charsets.UTF_8) shouldBe "Resource was not found."
-            }
-        }
+        response.statusCode shouldBe 200
 
-        "Server should persist connection on all HTTP/1.1 requests" {
-            val socket = Socket("0.0.0.0", 8093)
-            http.parseRequest("GET /hello HTTP/1.1\r\nHost: localhost").writeTo(socket.getOutputStream())
-            val response = http.parseResponse(socket.getInputStream()).eagerly(true)
+        // the server should have closed this Socket
+        socket.assertIsClosed()
+    }
 
-            response.statusCode shouldBe 200
+    @Test
+    fun `Server should persist connection on HTTP1_0 requests if 'Connection keep-alive' header is sent`() {
+        val socket = Socket("0.0.0.0", 8093)
+        http.parseRequest(
+            "GET /hello HTTP/1.0\r\n" +
+                    "Host: localhost\r\n" +
+                    "Connection: keep-alive"
+        ).writeTo(socket.getOutputStream())
+        val response = http.parseResponse(socket.getInputStream()).eagerly(true)
 
-            // the server should persist the connection
-            socket.assertIsOpen()
-        }
+        response.statusCode shouldBe 200
 
-        "Server should close connection on HTTP/1.1 requests if 'Connection: close' header is sent" {
-            val socket = Socket("0.0.0.0", 8093)
-            http.parseRequest(
-                "GET /hello HTTP/1.1\r\n" +
-                        "Host: localhost\r\n" +
-                        "Connection: close"
-            ).writeTo(socket.getOutputStream())
-            val response = http.parseResponse(socket.getInputStream()).eagerly(true)
+        // the server should persist the connection
+        socket.assertIsOpen()
+    }
 
-            response.statusCode shouldBe 200
+    @Test
+    fun `Server honours http client request desire to use 100-Continue`() {
+        val request = http.parseRequest(
+            "POST / HTTP/1.1\n" +
+                    "Host: localhost:8093\n" +
+                    "Expect: 100-continue\n" +
+                    "Content-Length: 10\n" +
+                    "\n" +
+                    "0123456789"
+        ).eagerly()
 
-            // the server should have closed this Socket
-            socket.assertIsClosed()
-        }
+        val socket = Socket(InetAddress.getLoopbackAddress(), 8093)
+        socket.soTimeout = 500
 
-        "Server should close connection on HTTP/1.0 requests" {
-            val socket = Socket("0.0.0.0", 8093)
-            http.parseRequest("GET /hello HTTP/1.0\r\nHost: localhost").writeTo(socket.getOutputStream())
-            val response = http.parseResponse(socket.getInputStream()).eagerly(true)
+        // don't send the body just yet
+        val out = socket.getOutputStream()
+        Thread {
+            request.startLine.writeTo(out)
+            request.headers.writeTo(out)
+        }.start()
 
-            response.statusCode shouldBe 200
+        val response100 = http.parseResponse(socket.getInputStream()).eagerly()
 
-            // the server should have closed this Socket
-            socket.assertIsClosed()
-        }
+        response100.statusCode shouldBe 100
+        response100.headers["Accept-100"] shouldBe listOf("True")
+        response100.body shouldBe beEmpty<EagerBodyReader>()
 
-        "Server should persist connection on HTTP/1.0 requests if 'Connection: keep-alive' header is sent" {
-            val socket = Socket("0.0.0.0", 8093)
-            http.parseRequest(
-                "GET /hello HTTP/1.0\r\n" +
-                        "Host: localhost\r\n" +
-                        "Connection: keep-alive"
-            ).writeTo(socket.getOutputStream())
-            val response = http.parseResponse(socket.getInputStream()).eagerly(true)
+        // Server accepted the body, send it
+        request.body.map { it.writeTo(out) }
 
-            response.statusCode shouldBe 200
+        // now the final response can be read
+        val response = http.parseResponse(socket.getInputStream()).eagerly()
 
-            // the server should persist the connection
-            socket.assertIsOpen()
-        }
-
-        "Server honours http client request desire to use 100-Continue" {
-            val request = http.parseRequest(
-                "POST / HTTP/1.1\n" +
-                        "Host: localhost:8093\n" +
-                        "Expect: 100-continue\n" +
-                        "Content-Length: 10\n" +
-                        "\n" +
-                        "0123456789"
-            ).eagerly()
-
-            val socket = Socket(InetAddress.getLoopbackAddress(), 8093)
-            socket.soTimeout = 500
-
-            // don't send the body just yet
-            val out = socket.getOutputStream()
-            Thread {
-                request.startLine.writeTo(out)
-                request.headers.writeTo(out)
-            }.start()
-
-            val response100 = http.parseResponse(socket.getInputStream()).eagerly()
-
-            response100.statusCode shouldBe 100
-            response100.headers["Accept-100"] shouldBe listOf("True")
-            response100.body shouldBe beEmpty<EagerBodyReader>()
-
-            // Server accepted the body, send it
-            request.body.map { it.writeTo(out) }
-
-            // now the final response can be read
-            val response = http.parseResponse(socket.getInputStream()).eagerly()
-
-            response.statusCode shouldBe 200
-            response.headers shouldHave validDateHeader()
-            response.body shouldBePresent {
-                it.asRawString(StandardCharsets.UTF_8) shouldBe "Thanks"
-            }
+        response.statusCode shouldBe 200
+        response.headers shouldHave validDateHeader()
+        response.body shouldBePresent {
+            it.asRawString(StandardCharsets.UTF_8) shouldBe "Thanks"
         }
     }
 
